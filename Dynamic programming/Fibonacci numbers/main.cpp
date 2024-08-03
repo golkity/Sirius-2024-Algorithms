4ba0db1057431b8842d2f1c3953f6e423d11c5ab
Finnik <73883383+golkity@users.noreply.github.com>
2024-08-03 19:05:19 +0300

    Update fib numbers main.cpp

--- Dynamic programming/Fibonacci numbers/main.cpp
+++ Dynamic programming/Fibonacci numbers/main.cpp
@@ -1,1 +1,20 @@
+#include "iostream"
+#include "vector"
 
+using namespace std;
+
+vector<int> dp;
+
+int F(int n){
+    if(dp[n]!=-1) return dp[n];
+    if(n<2) return n;
+    return dp[n] = F(n-1) + F(n-2);
+}
+
+int main(){
+    int n;
+    cin>>n;
+    dp.resize(n+1,-1);
+    cout<<F(n);
+    return 0;
+}
