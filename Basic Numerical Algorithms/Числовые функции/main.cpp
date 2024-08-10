9727e3a3ca24a5c05bee4e95e971af4a82422709
Finnik <73883383+golkity@users.noreply.github.com>
2024-08-10 12:05:19 +0300

    update main.cpp in folder Числовые Функции

--- Basic Numerical Algorithms/Числовые функции/main.cpp
+++ Basic Numerical Algorithms/Числовые функции/main.cpp
@@ -1,1 +1,27 @@
+#include "iostream"
+#include "cmath"
 
+using namespace std;
+
+int main(){
+    long long n;
+    cin>>n;
+
+    int sigma_0 = 0;
+    long long sigma_1 = 0;
+
+    for(long long i = 1;i<= sqrt(n);++i){
+        if(n%i == 0){
+            sigma_0+=2;
+            sigma_1+=i + n/i;
+            if(i==n/i){
+                sigma_0--;
+                sigma_1-=i;
+            }
+        }
+    }
+
+    cout<<sigma_0<<" "<<sigma_1<<endl;
+
+    return 0;
+}
