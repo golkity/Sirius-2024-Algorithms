8dc4d999d2ce31da173e6b343cd125e94087ced5
Finnik <73883383+golkity@users.noreply.github.com>
2024-08-10 11:21:02 +0300

    add Проверка числа на простоту

--- Basic Numerical Algorithms/Проверка числа на простоту/main.cpp
+++ Basic Numerical Algorithms/Проверка числа на простоту/main.cpp
@@ -1,1 +1,23 @@
+#include "iostream"
+#include "cmath"
+using namespace std;
 
+bool isPrime(int n){
+    for(int i = 2;i<= sqrt(n);++i){
+        if(n%i==0){
+            return false;
+        }
+    }
+    return true;
+}
+
+int main(){
+    int n;
+    cin>>n;
+    if(isPrime(n)){
+        cout<<"YES";
+    }else{
+        cout<<"NO";
+    }
+    return 0;
+}
