81ec62ecbdedcb1ccb4a3a16544db818049dc521
Finnik <73883383+golkity@users.noreply.github.com>
2024-08-22 16:11:26 +0300

    Update main.cpp

--- Бинарный поиск по ответу/Дипломы/main.cpp
+++ Бинарный поиск по ответу/Дипломы/main.cpp
@@ -1,1 +1,30 @@
+#include "bits-stdc++.h"
 
+using namespace std;
+#define ll long long
+
+bool ok(ll w, ll h, ll x, ll n) {
+    return (x / w) * (x / h) >= n;
+}
+
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    ll w,h,n;
+    cin>>w>>h>>n;
+    ll l = max(w,h);
+    ll r = l*n;
+    while(r-l>1){
+        ll m = (r+l)/2;
+        if(ok(w,h,m,n)){
+            r = m;
+        }else{
+            l = m;
+        }
+    }
+
+    cout<<r<<endl;
+
+    return 0;
+}
